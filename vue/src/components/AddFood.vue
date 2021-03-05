c1f81707c94a246f82372e87da1995f1e0532f52
Michael Drew <michaeljdrew14@gmail.com>
2021-03-05 16:43:28 -0500

    two components

--- vue/src/components/AddFood.vue
+++ vue/src/components/AddFood.vue
@@ -1,5 +1,40 @@
 <template>
-  <p>Add Food</p>
+  <form class="box form">
+        <b-field label="Food">
+            <b-input></b-input>
+        </b-field>       
+          <b-field label="Servings">
+            <b-select placeholder="Number of Servings" >
+                <option value="0.5">0.5</option>
+                <option value="1">1</option>
+                 <option value="1.5">1.5</option>
+                <option value="2">2</option>
+                <option value="2.5">2.5</option>
+                <option value="3">3</option>
+                <option value="3.5">3.5</option>
+                <option value="4">4</option>
+                <option value="4.5">4.5</option>
+                <option value="5">5</option>
+                <option value="5.5">5.5</option>
+                <option value="6">6</option>
+                <option value="6.5">6.5</option>
+                <option value="7">7 </option>
+            </b-select>
+        </b-field>       
+          <b-field label="Meal" >
+            <b-select placeholder="Type of Meal" expanded=true>
+                <option value="Breakfast">Breakfast</option>
+                <option value="Lunch">Lunch</option>
+                <option value="Dinner">Dinner</option>
+                <option value="Snack">Snack</option>
+                <option value="Dessert">Dessert</option>
+            </b-select>
+        </b-field>
+        
+        <button class="button is-link">ADD</button>
+        <!-- Add sex? -->
+       </form>
+
 </template>
 
 <script>
@@ -10,4 +45,7 @@ export default {
 
 <style scoped>
 
+.box{
+   display: inline-block;
+}
 </style>
--- vue/src/components/AddProfile.vue
+++ vue/src/components/AddProfile.vue
@@ -1,45 +1,88 @@
 <template>
-    <section>
-       <form class="box form">
-        <b-field label="Name">
-            <b-input></b-input>
-        </b-field>
+  <section>
+    <form class="box form">
+      <b-field label="Name">
+        <b-input></b-input>
+      </b-field>
 
-        <b-field label="Email">
-            <b-input ></b-input>
-        </b-field>
-         <b-field label="Age">
-            <b-numberinput min="13">
-            </b-numberinput>
-        </b-field>
-         <b-field label="Height">
-            <b-numberinput step="0.1" min="3.0">
-            </b-numberinput>
-        </b-field>
-        
-        
-         <b-field label="Current Weight">
-            <b-numberinput step="0.1">
-            </b-numberinput>
-        </b-field>
-        
-        <button class="button is-link">Submit</button>
-        <!-- Add sex? -->
-       </form>
+      <b-field label="Email">
+        <b-input></b-input>
+      </b-field>
+      <b-field label="Age">
+        <b-numberinput min="13"> </b-numberinput>
+      </b-field>
 
+      <b-field label="Height" 
+        ><b-field class="height" >
+        <b-select placeholder="Feet" expanded=true>
+          <option value="1">1 ft</option>
+          <option value="2">2 ft</option>
+          <option value="3">3 ft</option>
+          <option value="4">4 ft</option>
+          <option value="5">5 ft</option>
+          <option value="6">6 ft</option>
+          <option value="7">7 ft</option>
+        </b-select>
+        <b-select placeholder="Inches" expanded=true>
+          <option value="1">1 in</option>
+          <option value="2">2 in</option>
+          <option value="3">3 in</option>
+          <option value="4">4 in</option>
+          <option value="5">5 in</option>
+          <option value="6">6 in</option>
+          <option value="7">7 in</option>
+          <option value="8">8 in</option>
+          <option value="9">9 in</option>
+          <option value="10">10 in</option>
+          <option value="11">11 in</option>
+        </b-select>
+      </b-field>
+      </b-field>
 
-    </section>
+      <b-field label="Birthday">
+        <b-datepicker
+          placeholder="Click to select..."
+          :min-date="minDate"
+          :max-date="maxDate"
+        >
+        </b-datepicker>
+      </b-field>
+
+      <b-field label="Current Weight">
+        <b-numberinput step="0.1"> </b-numberinput>
+      </b-field>
+
+      <button class="button is-link">Submit</button>
+      <!-- Add sex? -->
+    </form>
+  </section>
 </template>
 
 <script>
 export default {
+  data() {
+    const today = new Date();
 
-}
+    return {
+      date: new Date(),
+      minDate: new Date(
+        today.getFullYear() - 80,
+        today.getMonth(),
+        today.getDate()
+      ),
+      maxDate: new Date(
+        today.getFullYear() + 18,
+        today.getMonth(),
+        today.getDate()
+      ),
+    };
+  },
+};
 </script>
 
 <style scoped>
-.box{
-   display: inline-block;
+.box {
+  display: inline-block;
 }
 
 </style>
