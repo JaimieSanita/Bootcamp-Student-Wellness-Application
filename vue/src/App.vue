e07981d4a0052415625d0447e199b6819c7aedb1
Jaimie Sanita <jsanita11@gmail.com>
2021-03-05 11:03:00 -0500

    added vue framework to components

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
+      <add-food>Add Food</add-food>
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+import AddFood from './components/AddFood.vue'
+export default {
+  components: { 
+    AddFood },
+
+}
+</script>
+
+<style scoped>
+
+</style>
