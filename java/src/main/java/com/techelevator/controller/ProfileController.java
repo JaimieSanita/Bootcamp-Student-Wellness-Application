9fe4d09cd6cf0a8b858690008c7df9bf295450a5
Jaimie Sanita <jsanita11@gmail.com>
2021-03-08 12:39:44 -0500

    registration and login functional

--- java/src/main/java/com/techelevator/controller/ProfileController.java
+++ java/src/main/java/com/techelevator/controller/ProfileController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.techelevator.dao.ProfileDAO;
 import com.techelevator.exception.ProfileNotFoundException;
 import com.techelevator.model.Profile;
@@ -24,30 +23,30 @@ public class ProfileController {
 		this.dao = dao;
 	}
 
-	@RequestMapping(path="/profile/id/{username}", method = RequestMethod.GET)
+	@RequestMapping(path = "/profile/id/{username}", method = RequestMethod.GET)
 	public int getProfileId(@PathVariable String username) {
 		return dao.findIdByUsername(username);
 	}
+
 	@ResponseStatus(HttpStatus.CREATED)
-	@RequestMapping(path="/profile/create", method = RequestMethod.POST)
+	@RequestMapping(path = "/profile/create", method = RequestMethod.POST)
 	public Profile createProfile(@RequestBody Profile newProfile, Principal principal) throws Exception {
 		String username = principal.getName();
 		int userId = this.dao.findIdByUsername(username);
-		if(userId != newProfile.getId()) {
-			//throw exception
+		if (userId != newProfile.getId()) {
+			// throw exception
 		}
 		return this.dao.create(newProfile);
-		
-		
+
 	}
+
 	@ResponseStatus(HttpStatus.OK)
-	@RequestMapping(path="/profile/update/{username}", method = RequestMethod.POST)
+	@RequestMapping(path = "/profile/update/{username}", method = RequestMethod.POST)
 	public void updateProfile(@RequestBody Profile profile, String username) throws ProfileNotFoundException {
 		this.dao.updateProfile(profile, username);
 	}
-	
-	
-	@RequestMapping(path="/profile/{username}", method = RequestMethod.GET)
+
+	@RequestMapping(path = "/profile/{username}", method = RequestMethod.GET)
 	public Profile getProfileFromUsername(@PathVariable String username) throws ProfileNotFoundException {
 		return this.dao.getByUsername(username);
 	}
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,89 +1,85 @@
 <template>
   <div id="register" class="text-center">
-     <section class="form">
-    <form class="box-form" @submit.prevent="register">
-      <h3 class="title">Let's Get You an Account!</h3>
-      <h1 class="subtitle">Please fill out the following fields to create your account</h1>
-      
-      <div class="alert alert-danger" role="alert" v-if="registrationErrors">
-        {{ registrationErrorMsg }}
-      
-      </div>
+    <section class="form">
+      <form class="form-register" @submit.prevent="register">
+        <h3 class="title">Let's Get You an Account!</h3>
+        <h1 class="subtitle">
+          Please fill out the following fields to create your account
+        </h1>
+        <div class="alert alert-danger" role="alert" v-if="registrationErrors">
+          {{ registrationErrorMsg }}
+        </div>
 
-      <b-field label="Username" ></b-field>
-      <b-input
-        type="text"
-        id="username"
-        class="form-control"
-        
-        v-model="user.username"
-        required
-        autofocus
-      ></b-input>
+        <b-field label="Username" ></b-field>
+        <b-input
+          type="text"
+          id="username"
+          class="form-control"
+          v-model="user.username"
+          required
+          autofocus
+        ></b-input>
 
-      <b-field label="Password" ></b-field>
-      <b-input
-        type="password"
-        id="password"
-        class="form-control"
-        
-        v-model="user.password"
-        required
-      ></b-input>
-      <b-field label=" Confirm Password" ></b-field>
-      <b-input
-        type="password"
-        id="confirmPassword"
-        class="form-control"
-     
-        v-model="user.confirmPassword"
-        required
-      ></b-input>
+        <b-field label="Password" ></b-field>
+        <b-input
+          type="password"
+          id="password"
+          class="form-control"
+          v-model="user.password"
+          required
+        ></b-input>
+        <b-field label=" Confirm Password" ></b-field>
+        <b-input
+          type="password"
+          id="confirmPassword"
+          class="form-control"
+          v-model="user.confirmPassword"
+          required
+        ></b-input>
 
         <b-field label="First Name">
-        <b-input ></b-input>
-      </b-field>
-       <b-field label="Last Name">
-        <b-input></b-input>
-      </b-field>
+          <b-input></b-input>
+        </b-field>
+        <b-field label="Last Name">
+          <b-input></b-input>
+        </b-field>
 
-      <b-field label="Age">
-        <b-numberinput min="13" type="is-info"> </b-numberinput>
-      </b-field>
+        <b-field label="Age">
+          <b-numberinput min="13" type="is-info" > </b-numberinput>
+        </b-field>
 
-      <b-field label="Height" 
-        ><b-field class="height" type="is-info">
-        <b-select placeholder="Feet" expanded=true >
-          <option value="1">1 ft</option>
-          <option value="2">2 ft</option>
-          <option value="3">3 ft</option>
-          <option value="4">4 ft</option>
-          <option value="5">5 ft</option>
-          <option value="6">6 ft</option>
-          <option value="7">7 ft</option>
-        </b-select>
-        <b-select placeholder="Inches" expanded=true>
-          <option value="1">1 in</option>
-          <option value="2">2 in</option>
-          <option value="3">3 in</option>
-          <option value="4">4 in</option>
-          <option value="5">5 in</option>
-          <option value="6">6 in</option>
-          <option value="7">7 in</option>
-          <option value="8">8 in</option>
-          <option value="9">9 in</option>
-          <option value="10">10 in</option>
-          <option value="11">11 in</option>
-        </b-select>
-      </b-field>
-      </b-field>
+        <b-field label="Height"
+          ><b-field class="height" type="is-info">
+            <b-select placeholder="Feet"  expanded=true>
+              <option value="1">1 ft</option>
+              <option value="2">2 ft</option>
+              <option value="3">3 ft</option>
+              <option value="4">4 ft</option>
+              <option value="5">5 ft</option>
+              <option value="6">6 ft</option>
+              <option value="7">7 ft</option>
+            </b-select>
+            <b-select placeholder="Inches" expanded=true>
+              <option value="1">1 in</option>
+              <option value="2">2 in</option>
+              <option value="3">3 in</option>
+              <option value="4">4 in</option>
+              <option value="5">5 in</option>
+              <option value="6">6 in</option>
+              <option value="7">7 in</option>
+              <option value="8">8 in</option>
+              <option value="9">9 in</option>
+              <option value="10">10 in</option>
+              <option value="11">11 in</option>
+            </b-select>
+          </b-field>
+        </b-field>
 
-      <b-field label="Current Weight">
-        <b-numberinput step="0.1" type="is-info"> </b-numberinput>
-      </b-field>
- 
- 
-      <!--
+        <b-field label="Current Weight">
+          <b-numberinput step="0.1" type="is-info"> </b-numberinput>
+        </b-field>
+
+        <!--
       <label for="username" class="sr-only">Username</label>
       <input
         type="text"
@@ -112,53 +108,53 @@
         required
       />
       -->
-     <div class="btn">
-      <b-button class="button is-link" type="is-info">Create Account</b-button>
-      </div>
-      <div class="link">
-       <router-link :to="{ name: 'login' }" class="have-an-account">Have an account?</router-link>
-      </div>
-      
-    </form>
-     </section>
+        <div class="btn">
+          <b-button v-on:click="register" class="btn btn-lg btn-primary btn-block" type="is-info"
+            >Create Account</b-button
+          >
+        </div>
+        <div class="link">
+          <router-link :to="{ name: 'login' }" class="have-an-account"
+            >Have an account?</router-link
+          >
+        </div>
+      </form>
+    </section>
   </div>
 </template>
 
 <script>
-
-import authService from '../services/AuthService';
+import authService from "../services/AuthService";
 
 export default {
-  name: 'register',
-  components: {
-
-
-  },
+  name: "register",
+  components: {},
   data() {
     return {
       user: {
-        username: '',
-        password: '',
-        confirmPassword: '',
-        role: 'user',
+        username: "",
+        password: "",
+        confirmPassword: "",
+        role: "user",
       },
       registrationErrors: false,
-      registrationErrorMsg: 'There were problems registering this user.',
+      registrationErrorMsg: "There were problems registering this user.",
     };
   },
   methods: {
     register() {
       if (this.user.password != this.user.confirmPassword) {
         this.registrationErrors = true;
-        this.registrationErrorMsg = 'Password & Confirm Password do not match.';
-      } else { //successful
+        this.registrationErrorMsg = "Password & Confirm Password do not match.";
+      } else {
+        //successful
         authService
           .register(this.user)
           .then((response) => {
             if (response.status == 201) {
               this.$router.push({
-                path: '/login',
-                query: { registration: 'success' }, //where to insert save profile
+                path: "/login",
+                query: { registration: "success" }, //where to insert save profile
               });
             }
           })
@@ -166,49 +162,48 @@ export default {
             const response = error.response;
             this.registrationErrors = true;
             if (response.status === 400) {
-              this.registrationErrorMsg = 'Bad Request: Validation Errors';
+              this.registrationErrorMsg = "Bad Request: Validation Errors";
             }
           });
       }
     },
     clearErrors() {
       this.registrationErrors = false;
-      this.registrationErrorMsg = 'There were problems registering this user.';
+      this.registrationErrorMsg = "There were problems registering this user.";
     },
   },
 };
 </script>
 
 <style scoped>
-
 section {
   display: flex;
   justify-content: center;
 }
-.button is-link{
+.button is-link {
   display: flex;
   justify-content: center;
 }
-.link{
+.link {
   display: block;
   justify-content: center;
 }
-.title, .subtitle{
-  margin-bottom: 1.0rem;
+.title,
+.subtitle {
+  margin-bottom: 1rem;
   justify-content: center;
   display: flex;
 }
-.link, .btn {
+.link,
+.btn {
   justify-content: center;
   display: flex;
 }
-.btn{
-  margin-top: 2.0rem;
-  margin-bottom: 1.0rem;
+.btn {
+  margin-top: 2rem;
+  margin-bottom: 1rem;
 }
-.have-an-account{
-  color:rgb(28, 139, 230);
+.have-an-account {
+  color: rgb(28, 139, 230);
 }
-
-
 </style>
