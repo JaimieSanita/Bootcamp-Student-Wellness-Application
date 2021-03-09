0b940366d3cf8456d3ba21d84f13daad0a4ac01b
Jaimie Sanita <jsanita11@gmail.com>
2021-03-09 13:53:41 -0500

    home page show's username

--- java/database/user_profile.sql
+++ java/database/user_profile.sql
@@ -68,7 +68,7 @@ Drop column create_date;
 
 Delete
 From users
-where user_id >3;
+where user_id >2;
 
 
 
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,46 +1,30 @@
 <template>
   <div class="home">
-    <h1>Welcome to {{Name}}'s TE Wellness Tracker!</h1>
+    <h1 class="title">Welcome to {{this.$store.state.user.username}}'s TE Wellness Tracker!</h1>
     <div class="container">
     <add-food/>
     <add-exercise/>
     </div>
-    <edit-food/>
-    <edit-profile/>
-    <food-summary/>
-    <profile-summary/>
-    <save-meal/>
-    <star-summary/>
-    <weight-summary/>
   </div>
 </template>
 
 <script>
 import AddExercise from '../components/AddExercise.vue';
 import AddFood from "../components/AddFood"
-import EditFood from '../components/EditFood.vue';
-import EditProfile from '../components/EditProfile.vue';
-import FoodSummary from '../components/FoodSummary.vue';
-import ProfileSummary from '../components/ProfileSummary.vue';
-import SaveMeal from '../components/SaveMeal.vue';
-import StarSummary from '../components/StarSummary.vue';
-import WeightSummary from '../components/WeightSummary.vue';
+
 export default {
   name: "home",
   components: { 
     AddFood,
-    EditFood,
-    EditProfile,
-    FoodSummary,
-    ProfileSummary,
-    SaveMeal,
-    StarSummary,
-    WeightSummary,
     AddExercise },
 };
 </script>
 <style scoped>
 .container{
+  display: flex;
+  justify-content: space-evenly;
+}
+h1{
   display: flex;
   justify-content: center;
 }
