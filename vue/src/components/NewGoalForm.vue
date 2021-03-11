34cda0c0626435cbf3697f566c5b7ebf615513af
Jaimie Sanita <jsanita11@gmail.com>
2021-03-11 17:43:23 -0500

    deleted ugly goal form

--- vue/src/components/NewGoalForm.vue
+++ vue/src/components/NewGoalForm.vue
@@ -1,39 +1,5 @@
 <template>
   <div class="container">
-    <form class="new-goal-form" v-on:submit.prevent="saveGoal">
-      <input
-        type="text"
-        class="category-input"
-        placeholder="Category"
-        v-model="newGoal.category"
-      />
-      <input
-        type="text"
-        class="activity-input"
-        placeholder="Activity"
-        v-model="newGoal.activity"
-      />
-      <input
-        type="text"
-        class="day-ssigned-input"
-        placeholder="Day Assigned"
-        v-model="newGoal.dayAssigned"
-      />
-      <input
-        type="text"
-        class="per-week-input"
-        placeholder="perWeek"
-        v-model="newGoal.perWeek"
-      />
-      <input
-        type="text"
-        class="duration-input"
-        placeholder="duration"
-        v-model="newGoal.duration"
-      />
-      <button>ADD</button>
-    </form>
-
     <form class="box form">
       <section>
         <b-field label="Let's set a goal!" type="is-info">
