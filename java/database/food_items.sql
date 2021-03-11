43cf20d03252b107a94eb7fe880c7049edba411c
Jasmine Joseph <jasmineljoseph2000@gmail.com>
2021-03-11 12:21:28 -0500

    same

--- java/database/food_items.sql
+++ java/database/food_items.sql
@@ -1,5 +1,6 @@
-BEGIN TRANSACTION;
+DROP TABLE IF EXISTS food_items, meals;
 
+BEGIN TRANSACTION;
 
 CREATE TABLE food_items
 (
@@ -14,18 +15,17 @@ meal_category                   varchar(32)  NOT NUll
 CREATE TABLE meals
 (
 meal_id      serial     NOT NULL PRIMARY KEY,
-meal_category              varchar(32) NOT NULL, 
+meal_category              varchar(32) NOT NULL,
 meal_name                  varchar(64)   NOT NULL
-
 );
 
-ALTER TABLE food_items
-ADD CONSTRAINT fk_food_id_id FOREIGN KEY (food_id) REFERENCES meals(meal_id);
-
-ALTER TABLE meals
-ADD CONSTRAINT fk_meal_id_id FOREIGN KEY (meal_id) REFERENCES users(user_id);
-
-SELECT * FROM food_items;
-SELECT * FROM meals; 
-
 COMMIT;
+
+//ALTER TABLE food_items
+//ADD CONSTRAINT fk_food_id_id FOREIGN KEY (food_id) REFERENCES meals(meal_id);
+
+//ALTER TABLE meals
+//ADD CONSTRAINT fk_meal_id_id FOREIGN KEY (meal_id) REFERENCES users(user_id);
+
+//SELECT * FROM food_items;
+//SELECT * FROM meals;
