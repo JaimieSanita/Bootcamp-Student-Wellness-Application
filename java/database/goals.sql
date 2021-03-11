9ed2a9f86f98641c2beee8525e34bad972e7f3fc
Michael Drew <michaeljdrew14@gmail.com>
2021-03-11 15:12:59 -0500

    goals

--- java/database/goals.sql
+++ java/database/goals.sql
@@ -3,6 +3,12 @@ DROP TABLE IF EXISTS user_goals;
 
 BEGIN TRANSACTION;
 
+CREATE TABLE goal_category
+(
+goal_category_id      INT GENERATED ALWAYS AS IDENTITY ,
+category      varchar(128)    NOT NULL,
+CONSTRAINT PK_goal_category PRIMARY KEY (goal_category_id)
+);
 
 
 CREATE TABLE user_goals
@@ -10,24 +16,18 @@ CREATE TABLE user_goals
 user_goals_id      INT GENERATED ALWAYS AS IDENTITY ,
 user_id int NOT NULL, 
 date_assigned DATE NOT NULL,
-category      varchar(128)    NOT NULL,
+category_id      int    NOT NULL,
 activity      varchar(128) NOT NULL,
 times_per_week      int    NOT NULL,
 duration          int   NOT NULL,
 is_completed         boolean            NOT NULL,
 CONSTRAINT PK_goals PRIMARY KEY (user_goals_id),
-CONSTRAINT FK_goals_user FOREIGN KEY(user_id) REFERENCES users(user_id)
+CONSTRAINT FK_goals_user FOREIGN KEY(user_id) REFERENCES users(user_id),
+CONSTRAINT FK_goals_category FOREIGN KEY(category_id) REFERENCES goal_category(goal_category_id)
+
 );
 
 
-CREATE TABLE goal_category
-(
-goal_category_id      INT GENERATED ALWAYS AS IDENTITY ,
-user_id int,
-category      varchar(128)    NOT NULL,
-CONSTRAINT PK_goal_category PRIMARY KEY (goal_category_id),
-CONSTRAINT FK_goals_user FOREIGN KEY(user_id) REFERENCES user_goals(user_goals_id)
-);
 
 
 GRANT SELECT, INSERT, UPDATE, DELETE
@@ -39,12 +39,15 @@ GRANT SELECT, INSERT, UPDATE, DELETE
 ON user_goals 
 TO final_capstone_appuser;
 
+INSERT INTO goal_category(category) Values ('exercise');
+
+INSERT INTO user_goals(user_id, date_assigned, category_id, activity, times_per_week, duration, is_completed)
+VALUES (1, '05/24/2020' , 1, 'running', 2, 10, false);
 
 
-INSERT INTO user_goals(user_id, date_assigned, category, activity, times_per_week, duration, is_completed)
-VALUES (1, '05/24/2020' , 'exercise', 'burpees', 3, 30, false);
 
-INSERT INTO goal_category(category,user_id) Values ('exercise',1);
+
+
 
 Select * from goal_category;
 SELECT * FROM user_goals;
@@ -53,6 +56,7 @@ ROLLBACK;
 
 
 COMMIT;
+Select * from user_goals where user_id = 1;
 
 SELECT user_goals_id, date_assigned, category, activity, times_per_week, duration, is_completed
 FROM user_goals
@@ -64,5 +68,7 @@ UPDATE  user_goals SET user_id = 1, date_assigned ='1/2/13/', category='running'
 FROM user_goals AS g
 JOIN users
 ON users.user_id = g.user_id
-WHERE username = 'user';
+WHERE user_id= 1;
 
+
+SELECT count(*) FROM user_goals AS ug JOIN users AS u ON ug.user_id = u.user_id WHERE u.username= 'user' AND ug.user_goals_id =1;
