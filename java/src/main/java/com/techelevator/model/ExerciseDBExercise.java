477c4c1716f6f37e935b7773cdb9cfc610950572
Shomi Edeki <shomie373@gmail.com>
2021-03-11 11:49:57 -0500

    added ExerciseDBExercise

---
+++ java/src/main/java/com/techelevator/model/ExerciseDBExercise.java
@@ -0,0 +1,39 @@
+package com.techelevator.model;
+
+public class ExerciseDBExercise {
+	
+	private String name;
+	private int duration_min;
+	private int nf_calories;
+	
+	public ExerciseDBExercise() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDuration_min() {
+		return duration_min;
+	}
+
+	public void setDuration_min(int duration_min) {
+		this.duration_min = duration_min;
+	}
+
+	public int getNf_calories() {
+		return nf_calories;
+	}
+
+	public void setNf_calories(int nf_calories) {
+		this.nf_calories = nf_calories;
+	}
+	
+	
+
+}
