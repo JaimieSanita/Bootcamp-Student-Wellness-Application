e086087cbcf8a7ef11f7a59b4eabcde00b2e2c83
Shomi Edeki <shomie373@gmail.com>
2021-03-15 11:09:02 -0400

    foodDAO and FoodSqlDAO

--- java/src/main/java/com/techelevator/dao/FoodSqlDAO.java
+++ java/src/main/java/com/techelevator/dao/FoodSqlDAO.java
@@ -1,5 +1,7 @@
 package com.techelevator.dao;
 
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -38,6 +40,13 @@ public class FoodSqlDAO implements FoodDAO {
 					+ "food_name = ?, meal_category = ?" + "WHERE food_id = ?";
 			// TODO Auto-generated method stub
 			return jdbcTemplate.update(query, food.getFoodName(), food.getCalories(), food.getServings(), foodId);
+		}
+
+
+		@Override
+		public List<Food> findAll() {
+			// TODO Auto-generated method stub
+			return null;
 		}
 		
 		
