4e64eeaddec404270947cf9fa3ceb6e2da5d6255
Shomi Edeki <shomie373@gmail.com>
2021-03-15 11:43:35 -0400

    foodDAO and FoodSqlDao

--- java/src/main/java/com/techelevator/model/Food.java
+++ java/src/main/java/com/techelevator/model/Food.java
@@ -1,47 +1,65 @@
 package com.techelevator.model;
 
 public class Food {
-	
+
 	private String foodName;
 	private int servings;
 	private String mealCategory;
 	private int calories;
-	
-	
+	private int foodId;
+
 	public int getCalories() {
 		return calories;
 	}
+
 	public void setCalories(int calories) {
 		this.calories = calories;
 	}
+
 	public String getFoodName() {
 		return foodName;
 	}
+
 	public void setFoodName(String foodName) {
 		this.foodName = foodName;
 	}
+
 	public int getServings() {
 		return servings;
 	}
+
 	public void setServings(int servings) {
 		this.servings = servings;
 	}
+
 	public String getMealCategory() {
 		return mealCategory;
 	}
+
 	public void setMealCategory(String mealCategory) {
 		this.mealCategory = mealCategory;
 	}
+
+	public int getFoodId() {
+		return foodId;
+	}
+
+	public void setFoodId(int foodId) {
+		this.foodId = foodId;
+	}
+
 	public Food(String foodName, int servings, String mealCategory, int calories) {
-		
+
 		this.foodName = foodName;
 		this.servings = servings;
 		this.mealCategory = mealCategory;
 		this.calories = calories;
 	}
-	public Food() {}
-	
-	
+
+	public Food() {
+
 	}
+
 	
 
+}
