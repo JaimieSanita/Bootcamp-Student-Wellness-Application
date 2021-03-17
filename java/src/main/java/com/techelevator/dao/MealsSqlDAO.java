884010f276aa3cce3d2bd794cad29a2748e6c956
Shomi Edeki <shomie373@gmail.com>
2021-03-17 12:50:30 -0400

    JDBC Meals

--- java/src/main/java/com/techelevator/dao/MealsSqlDAO.java
+++ java/src/main/java/com/techelevator/dao/MealsSqlDAO.java
@@ -42,25 +42,41 @@ public class MealsSqlDAO implements MealsDAO {
 
 	}
 
-	private Meals mapRowToMeal(SqlRowSet r) {
+	private Meals mapRowToMeal(SqlRowSet rs) {
 		// TODO Auto-generated method stub
-		return null;
+		Meals meals = new Meals();
+		meals.setMealId(rs.getInt("meal_id"));
+		meals.setManuallyInput(rs.getBoolean("manually_input"));
+		meals.setMealCalories(rs.getInt("total_calories"));
+		meals.setMealCategory(rs.getString("meal_category"));
+		meals.setMealName(rs.getNString("meal_name"));
+		meals.setUserId(rs.getInt("user_id"));
+		return meals;
 	}
 
 	@Override
 	public int updateMeal(Meals meals, int mealId) {
 		// TODO Auto-generated method stub
-		return 0;
+		String query = "UPDATE meals SET meal_id = ?, meal_category = ?, meal_name = ?, total_calories = ?, quick_saved_meal = ?"
+				+ "manually_input = ?, user_id = ?";
+		return jdbcTemplate.update(query, meals.getMealCalories(), meals.getMealCategory(), 
+				meals.getMealId(), meals.getMealName(), meals.getUserId(), mealId);
 	}
 
 	@Override
 	public Meals findMealByName(String mealName) {
 		// TODO Auto-generated method stub
+		for(Meals meals : this.listAllMeals()) {
+			if(meals.getMealName().toLowerCase().equals(mealName.toLowerCase()));
+			return meals;
+		}
 		return null;
 	}
 
 	@Override
 	public void delete(int MealId) {
+		String query = "DELETE FROM meals WHERE meal_id = ?";
+		this.jdbcTemplate.update(query, MealId);
 		// TODO Auto-generated method stub
 
 	}
