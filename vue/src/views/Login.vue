1c86acc6b1680d2313d1465437dc0697f5c10203
Jaimie Sanita <jsanita11@gmail.com>
2021-03-18 12:54:13 -0400

    login page styled

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,81 +1,100 @@
 <template>
-<div class="all">
-  <div class="header">
-    <div class="banner-container">
-      <div style="position:relative; left:-780px;"        id="nav">
-      <router-link id="link" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div>
-   
-      <h3 class="banner-title">Never forget, as you elevate, Wellovate!</h3>
-    </div>
-    <div class="welcome-container">
-      <img class="logo-image" src="../assets/logo.png">
-      <h1 class="title">
-        Welcome to The Tech Elevator Wellovater!
-      </h1>
-    </div>
-  </div>
-<div class="container">
-   <img class="background" src="https://media.istockphoto.com/photos/elevator-picture-id180821428?k=6&m=180821428&s=612x612&w=0&h=SLXASBb52_ekAmTp4qz5i2_VYehC4X2qA9DsuySCe7A=">
-  <div id="login" class="text-center">
-    <div class="banner-container">
-    <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">Please Sign In or Sign Up to Begin Your Wellness</h1>
-      <div class="alert alert-danger" role="alert" v-if="invalidCredentials">
-        Invalid username and password!
+  <div id="page-container" class="all">
+    <div id="content-wrap">
+    <div class="header">
+      <div class="banner-container">
+        <div style="position: relative; left: -780px" id="nav">
+          <router-link
+            id="link"
+            v-bind:to="{ name: 'logout' }"
+            v-if="$store.state.token != ''"
+            >Logout</router-link
+          >
+        </div>
+
+        <marquee behavior="scroll" direction="left" class="banner-title">Never forget, as you elevate, Wellovate!</marquee>
       </div>
-      <div
-        class="alert alert-success"
-        role="alert"
-        v-if="this.$route.query.registration"
-      >
-        Thank you for registering, please sign in.
+      <div class="welcome-container">
+        <img class="logo-image" src="../assets/logo.png" />
+        <h1 class="title">Welcome to Tech Elevator Wellovater!</h1>
       </div>
-      <div class="control">
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-        
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      </div>
+    </div>
+    <div class="container">
      
-      <div class= "login-buttons">
-         <button id="up" class="button is-primary is-small is-focused" type="submit">
-      <router-link class="sign-up" :to="{ name: 'register' }">Sign Up</router-link>
-         </button>
-      <button id ="in" class="button is-primary is-small is-focused" type="submit">Sign in</button>
+      <div id="login" class="text-center">
+        <div class="box" box-shadow="0 0.5em 1em -0.125em rgba($scheme-invert, 0.1), 0 0px 0 1px rgba($scheme-invert, 0.02)">
+          <form class="form-signin" @submit.prevent="login">
+            <h1 class="title">Log In</h1>
+            <div
+              class="alert alert-danger"
+              role="alert"
+              v-if="invalidCredentials"
+            >
+              Invalid username and password!
+            </div>
+            <div
+              class="alert alert-success"
+              role="alert"
+              v-if="this.$route.query.registration"
+            >
+              Thank you for registering, please sign in.
+            </div>
+            <div class="control">
+              <b-input
+                type="text"
+                id="username"
+                class="form-control"
+                placeholder="Username"
+                v-model="user.username"
+                required
+                autofocus
+              />
+
+              <b-input
+                type="password"
+                id="password"
+                class="form-control"
+                placeholder="Password"
+                v-model="user.password"
+                required
+              />
+            </div>
+
+            <div class="login-button">
+              <b-button
+                id="in"
+                class="button is-primary is-focused"
+                :expanded="true"
+                type="submit"
+                v-on:click.prevent="login"
+                >Sign in</b-button
+              >
+            </div>
+           
+            <div class="register-container">
+               <p class="register-question">Don't have an account?</p>
+                <router-link style="color: #00ADEE" class="sign-up" :to="{ name: 'register' }"
+                  >Sign Up</router-link
+                >
+             
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-    </form>
     </div>
- </div>
-  </div >
-  <te-footer/>
+    <te-footer id="footer" />
   </div>
 </template>
 
 <script>
-import TeFooter from '../components/TeFooter.vue';
+import TeFooter from "../components/TeFooter.vue";
 import authService from "../services/AuthService";
 import profileService from "../services/ProfileService";
 
 export default {
   name: "login",
-  components: {TeFooter},
+  components: { TeFooter },
   data() {
     return {
       user: {
@@ -94,18 +113,16 @@ export default {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
             //TODO return calling the profile api
-           return profileService
-              .get(this.user.username);
-             
+            return profileService.get(this.user.username);
           }
-        }) 
+        })
         .then((response) => {
-                if (response.status === 200) {
-                  //.then if valid profile call vuex mutation to set profile
-                  this.$store.commit("UPDATE_PROFILE", response.data);
-                  this.$router.push("/");
-                }
-              })
+          if (response.status === 200) {
+            //.then if valid profile call vuex mutation to set profile
+            this.$store.commit("UPDATE_PROFILE", response.data);
+            this.$router.push("/");
+          }
+        })
         .catch((error) => {
           const response = error.response;
 
@@ -118,7 +135,6 @@ export default {
 };
 </script>
 <style scoped>
-
 .container {
   display: block;
   margin-left: auto;
@@ -131,7 +147,7 @@ export default {
   background-color: #00adee;
   display: flex;
   justify-content: flex-start;
-font-family: proxima-nova;
+  font-family: proxima-nova;
   height: 90px;
   margin: 10px;
 }
@@ -140,54 +156,77 @@ font-family: proxima-nova;
   color: white;
   display: flex;
   justify-content: center;
- font-family: proxima-nova;
-}
-.title{
-  color: white;
-  display: flex;
-  padding: 30px;
-  position: relative;
-  padding-left: 28%;
   font-family: proxima-nova;
 }
-#submit{
+.box {
+  margin-top: 50px;
+  padding-top: 10px;
+  background-color: #c2c0c0;
+  color: white;
+  display: flex;
+  justify-content: center;
+  font-family: proxima-nova;
+  padding-bottom: 20px;
+
+  
+}
+.title {
+  color: white;
+  display: flex;
+  position: relative;
+  font-family: proxima-nova;
+  padding: 30px;
+  padding-left: 25%;
+}
+#submit {
   background-color: #00adee;
   font-family: proxima-nova;
 }
-.sign-up{
+.sign-up:hover {
   color: white;
   font-family: proxima-nova;
+  font-weight: bold;
 }
-.button{
+.button {
   font-family: proxima-nova;
   margin: 2px;
 }
-#username{
-  border-radius: 5px;
-  font-family: proxima-nova;
- 
-}
-#password{
+#username {
   border-radius: 5px;
   font-family: proxima-nova;
 }
-.all{
+#password {
+  border-radius: 5px;
   font-family: proxima-nova;
+}
+.all {
+  font-family: proxima-nova;
+}
+.register-container{
+  padding-top: 10px;
 }
 
-.login-buttons{
+.login-buttons {
   text-decoration-color: white;
   display: flex;
   justify-content: center;
   padding-top: 15px;
 }
-#footer {
-   position:absolute;
-   bottom:0;
-   width:100%;
-   height:60px;   /* Height of the footer */
-   background: white;
+
+#page-container {
+  position: relative;
+  min-height: 100vh;
 }
 
+#content-wrap {
+  padding-bottom: 2.5rem;   
+}
 
+#footer {
+  font-size: small;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 2.5rem;           
+}
 </style>
